f843c7241ff69506cae1af37c72726aa25acc1d1
Alastair Paragas <alastairparagas@gmail.com>
2017-08-04 15:14:09 +0200

    KeywordProximityRanker: change rank() interface

--- src/main/scala/ZenodoAddon/Graph/KeywordProximityRanker.scala
+++ src/main/scala/ZenodoAddon/Graph/KeywordProximityRanker.scala
@@ -4,5 +4,6 @@ package ZenodoAddon.Graph
 trait KeywordProximityRanker[GraphTypeVar]
 {
   def rank(pgxGraph: GraphTypeVar,
-           keyword: String): Seq[(String, Double)]
+           keyword: String,
+           take: Int): List[String]
 }
